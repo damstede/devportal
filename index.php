5e6acacb1bc2c173f353bdd0b10728ab9a818276
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-10-28 09:26:10 +0100

    eerste commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?PHP
+    header("Location: portal.php", 301);
+    exit();
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>Doorgaan...</title>
+</head>
+<body>
+    <h1>Doorgaan...</h1>
+    <p>Je wordt automatisch doorgestuurd...<br/><small>Klik <a href="portal.php">shier</a> als dit niet gebeurt.</small></p>
+</body>
+</html>
---
+++ portal.php
@@ -0,0 +1,13 @@
+<?PHP
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>Damstede Device Portaal</title>
+</head>
+<body>
+    <h1>Werk in uitvoering</h1>
+    <p>Kom binnenkort nog eens terug!</p>
+</body>
+</html>
