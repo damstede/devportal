e9587c051c89a089b6ea7e8aa8d88cb5e20b0bc5
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-10-28 10:31:30 +0100

    kleine opmaak toegevoegd

--- portal.php
+++ portal.php
@@ -5,9 +5,16 @@
 <html lang="nl">
 <head>
     <title>Damstede Device Portaal</title>
+    <link rel="stylesheet" href="opmaak.css" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+	<link rel="icon" type="image/ico" href="favicon.ico" />
 </head>
 <body>
-    <h1>Werk in uitvoering</h1>
-    <p>Kom binnenkort nog eens terug!</p>
+    <header>
+        <h1 id="pagetitle">Device Portaal</h1>
+    </header>
+    <div id="content">
+        <p>Test</p>
+    </div>
 </body>
 </html>
