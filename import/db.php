1035c2ae3f384d392b3a163e7a8379a2193b996a
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-11-11 10:44:02 +0100

    begin databasefuncties toegevoegd

---
+++ import/db.php
@@ -0,0 +1,87 @@
+<?PHP
+    // nogit.php contains $dbUsername, $dbPassword and $dbName
+    require_once("nogit.php");
+
+    class DamstedeDB {
+        private $connection = null;
+
+        function __construct() {
+            global $dbUsername, $dbPassword, $dbName;
+            $this->connection = mysqli_connect("localhost", $dbUsername, $dbPassword, $dbName) or returnError("MySQL connection error: " . mysqli_connect_error());
+            mysqli_set_charset($this->connection, 'utf8mb4');
+        }
+
+        private function makeSafe($str) {
+            $str = stripslashes(trim($str));
+            $str = mysqli_real_escape_string($this->connection, strip_tags($str));
+            return $str;
+        }
+
+        private function runQuery($query) {
+            return mysqli_query($this->connection, $query);
+        }
+
+        private function getStartAndEndDate($year, $week) {
+            // from https://stackoverflow.com/questions/4861384/php-get-start-and-end-date-of-a-week-by-weeknumber
+            $dto = new DateTime();
+            $dto->setISODate($year, $week);
+            $ret = array();
+            array_push($ret, $dto->format('Y-m-d'));
+            $dto->modify('+6 days');
+            array_push($ret, $dto->format('Y-m-d'));
+            return $ret;
+        }
+
+        public function getDeviceCarts() {
+            $carts = array();
+            $result = $this->runQuery("SELECT id FROM damstede.devicecarts");
+            if ($result != false) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $cart = $this->getDeviceCart($row["id"]);
+                    if (!empty($cart)) {
+                        array_push($carts, $cart);
+                    }
+                }
+            }
+            return $carts;
+        }
+
+        public function getDeviceCart($cartId) {
+            $cart = false;
+            $result = $this->runQuery("SELECT * FROM damstede.devicecarts WHERE id='".$this->makeSafe($cartId)."' LIMIT 1");
+            if ($result != false) {
+                if (mysqli_num_rows($result) > 0) {
+                    $cart = mysqli_fetch_assoc($result);
+                    $cart["id"] = intval($cart["id"]);
+                    $cart["dev_amount"] = intval($cart["dev_amount"]);
+                    $cart["available"] = (intval($cart["available"]) > 0);
+                }
+            }
+            return $cart;
+        }
+
+        private function formatReservation($res) {
+            $res["id"] = intval($res["id"]);
+            $res["cart_id"] = intval($res["cart_id"]);
+            $res["registered_on"] = strtotime($res["registered_on"]);
+            $res["day"] = intval(date("w", strtotime($res["date"]))) - 1;
+            $res["hour"] = intval($res["hour"]);
+            $res["cancelled"] = (intval($res["cancelled"]) > 0);
+            return $res;
+        }
+
+        public function getCartReservations($cartId, $year, $week) {
+            $reservations = array();
+            $week = $this->getStartAndEndDate($year, $week);
+            $sql = "SELECT * FROM damstede.cartreservations WHERE DATE(date) >= STR_TO_DATE('".$this->makeSafe($week[0])."', '%Y-%m-%d') AND DATE(date) <= STR_TO_DATE('".$this->makeSafe($week[1])."', '%Y-%m-%d')";
+            if (!empty($cartId)) {
+                $sql .= " AND cart_id=".intval($cartId);
+            }
+            $result = $this->runQuery($sql);
+            while ($row = mysqli_fetch_assoc($result)) {
+                array_push($reservations, $this->formatReservation($row));
+            }
+            return $reservations;
+        }
+    }
+?>
---
+++ import/dschedule.php
@@ -0,0 +1,57 @@
+<?PHP
+    header('Content-Type: text/html; charset=utf-8');
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+
+    $data = array();
+	$data["type"] = "error";
+	$data["message"] = "Onbekende error";
+	$data["data"] = array();
+	
+	function returnError($msg) {
+		global $data;
+		$data["type"] = "error";
+		$data["message"] = $msg;
+		$data["data"] = array();
+		header('Content-Type: application/json; charset=utf-8');
+		echo json_encode($data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+		die();
+	}
+	
+	function returnWarning($msg) {
+		global $data;
+		$data["type"] = "warning";
+		$data["message"] = $msg;
+		$data["data"] = array();
+		header('Content-Type: application/json; charset=utf-8');
+		echo json_encode($data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+		die();
+	}
+	
+	function returnData($msg, $stuff) {
+		global $data;
+		$data["type"] = "success";
+		$data["message"] = $msg;
+		$data["data"] = $stuff;
+		header('Content-Type: application/json; charset=utf-8');
+		echo json_encode($data, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+		die();
+    }
+
+    session_start();
+
+    if (!isset($_SESSION["zermelo_access_token"]) || empty($_SESSION["zermelo_access_token"])) {
+        returnError("Je bent niet (meer) ingelogd! Log opnieuw in.");
+    }
+    else {
+        if (isset($_GET["year"]) && !empty($_GET["year"]) && isset($_GET["week"]) && !empty($_GET["week"])) {
+            require_once("db.php");
+            $damstedeDB = new DamstedeDB();
+            returnData("Reserveringen gevonden", $damstedeDB->getCartReservations(intval($_GET["cart"]), intval($_GET["year"]), intval($_GET["week"])));
+        }
+        else {
+            returnError("Missende data: GET year en GET week moeten worden aangegeven. GET cart is niet verplicht, maar kan wel handig zijn om enkel voor een bepaalde kar reserveringen op te vragen.");
+        }
+    }
+?>
